6a1e00e60a9b73fde79e93e7096c6ed0671d6afd
LINK-INNOVATION\bkuzucu <bedirhan.kuzucu@ituc.de>
2024-02-04 14:10:25 +0100

    home page and sidebare created

---
+++ src/app/template.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+
+function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      className="h-screen"
+      initial={{ y: 20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.75 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default Template;
+
+{
+  /* <motion.div
+      className="box"
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 180, 180, 0],
+        borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
+    ></motion.div> */
+}
